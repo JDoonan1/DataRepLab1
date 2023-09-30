c70bb38dbb3c6d010136073378d9f33654b39c7a
James Doonan <124049432+JDoonan1@users.noreply.github.com>
2023-09-30 12:26:26 +0100

    Update ex2pt4.js

--- ex2pt4.js
+++ ex2pt4.js
@@ -3,12 +3,14 @@ const ages = [25, 31, 42, 77];
 
 //Calls a defined callback function on each element of an array, and returns an array that contains the results.
 let PartD = ages.map((element)=>{
+ //if statement to compare elemnt values to 70
    if(element < 70){
     return element*2;
    } else{
     return element;
    }
 });
+//print solution to console
 console.log(PartD);
 
 
